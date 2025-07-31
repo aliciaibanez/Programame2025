11d49c4f4f9bdb87b40e69ef756d5c2b61eece77
aliciaibanez <alicia.ibanez.escudero@gmail.com>
2025-07-31 12:43:57 +0200

    axtualizacion

--- Nivel 1/Ejercicio2/Ejercicio2.java
+++ Nivel 1/Ejercicio2/Ejercicio2.java
@@ -1,26 +1,30 @@
 import java.util.Scanner;
 
-public class Ejercicio2
-{
-    public static void main (String[] args) {
+public class Ejercicio2 {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
-            double distanciaRecorrida = scanner.nextDouble();
-            double velocidadMaxima = scanner.nextDouble();
-            double segundosEnRecorrer = scanner.nextDouble();
-            if (distanciaRecorrida == 0 && velocidadMaxima == 0 && segundosEnRecorrer == 0) {
+            double espacio = scanner.nextDouble() / 1000.0; // metros -> km
+            double velocidadMaxima = scanner.nextDouble(); // km/h
+            double tiempo = scanner.nextDouble() / 3600.0; // segundos -> horas
+            double velocidadCoche = espacio / tiempo;
+            if (espacio == 0 && velocidadMaxima == 0 && tiempo == 0) {
                 break;
             }
-            double velocidadCoche = distanciaRecorrida/segundosEnRecorrer;
-            if (velocidadCoche < velocidadMaxima) {
+            else if (velocidadCoche < velocidadMaxima) {
                 System.out.println("OK");
-            }
-            else if (velocidadCoche < (velocidadMaxima * 1.2)) {
+            } 
+            else if ((velocidadCoche + velocidadMaxima * 0.2) < velocidadMaxima) {
                 System.out.println("MULTA");
-            }
-            else if (velocidadCoche >= (velocidadMaxima * 1.2)) {
+            } 
+            else if ((velocidadCoche + velocidadMaxima * 0.2)> velocidadMaxima) {
                 System.out.println("PUNTOS");
             }
+            else {
+                System.out.println("ERROR");
+            }
         }
+        scanner.close();
     }
 }
